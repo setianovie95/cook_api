d9394d315cdb23e7c610be8cd63cac9738bd02d9
setianovie95 <adibest25@gmail.com>
2023-12-01 11:31:59 +0700

    filter js

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -5,3 +5,32 @@
  */
 
 "use strict";
+
+
+
+
+/**Import */
+import { fetchData } from "./api.js";
+import { $skeletonCard, cardQueries } from "./global.js";
+import { getTime } from "./module.js";
+
+/**Accordion */
+const /**Node List */ $accordions = document.querySelectorAll("[data-accordion]");
+
+/**
+ * 
+ * @param {NodeList} $element Accordion Node
+ */
+
+const initAccordion = function ($element) {
+    const /**Node Element */ $button = $element.querySelector("[data-accordion-btn]");
+    let isExpanded = false;
+
+    $button.addEventListener("click", function () {
+        isExpanded = isExpanded ? false : true;
+        this.setAttribute("aria-expanded", isExpanded);
+    });
+}
+
+
+for (const $accordion of $accordions) initAccordion($accordion);
