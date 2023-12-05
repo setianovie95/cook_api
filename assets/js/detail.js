9487496eb30554d36b2c01b793383798ab3a863a
setianovie95 <adibest25@gmail.com>
2023-12-05 14:40:29 +0700

    saved recipes

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -19,10 +19,8 @@ ACCESS_POINT += `/${window.location.search.slice(window.location.search.indexOf(
 
 fetchData(null, data => {
 
-    console.log(data);
-
     const {
-        image: { LARGE, REGULAR, SMALL, THUMBNAIL },
+        images: { LARGE, REGULAR, SMALL, THUMBNAIL },
         label: title,
         source: author,
         ingredients = [],
@@ -52,7 +50,7 @@ fetchData(null, data => {
 
         let /**String */ type = "";
 
-        if (cuisineType.includes(tas)) {
+        if (cuisineType.includes(tag)) {
             type = "cuisineType";
         } else if (dietLabels.includes(tag)) {
             type = "diet";
@@ -61,7 +59,74 @@ fetchData(null, data => {
         }
 
         tagElements += `
+            <a href="./recipes.html?${type}=${tag.toLowerCase()}" class="filter-chip label-large has-state">${tag}</a>
         `;
     });
+
+    ingredientLines.map(ingredient => {
+        ingredientItems += `
+            <li class="ingr-item">${ingredient}</li>
+        `;
+    });
+
+    $detailContainer.innerHTML = `
+        <figure class="detail-banner img-holder">
+            <img src="${bannerUrl}" width="${width}" height="${height}"
+                alt="${title}" class="img-cover">
+        </figure>
+
+        <div class="detail-content">
+
+            <div class="title-wrapper">
+
+                <h1 class="display-small">${title ?? "Untitled"}</h1>
+
+                <button class="btn btn-secondary has-state has-icon ${isSaved ? "saved" : "removed"}" onclick="saveRecipe(this, '${recipeId}')" >
+
+                    <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
+
+                    <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
+
+                    <span class="label-large save-text">Save</span>
+                    <span class="label-large unsaved-text">Unsaved</span>
+
+                </button>
+
+            </div>
+
+            <div class="detail-author label-large">
+                <span class="span">by</span> ${author}
+            </div>
+
+            <div class="detail-stats">
+
+                <div class="stats-item">
+                    <span class="display-medium">${ingredients.length}</span>
+                    <span class="label-medium">Ingredients</span>
+                </div>
+
+                <div class="stats-item">
+                    <span class="display-medium">${getTime(cookingTime).time || "<1"}</span>
+                    <span class="label-medium">${getTime(cookingTime).timeUnit}</span>
+                </div>
+
+                <div class="stats-item">
+                    <span class="display-medium">${Math.floor(calories)}</span>
+                    <span class="label-medium">Calories</span>
+                </div>
+
+            </div>
+
+            ${tagElements ? `<div class="tag-list">${tagElements}</div>` : ""} 
+
+            <h2 class="title-medium ingr-title">
+                Ingredients
+                <span class="label-medium">for ${servings} Servings</span>
+            </h2>
+
+            ${ingredientItems ? `<ul class="body-large ingr-list">${ingredientItems}</ul>` : ""}
+
+        </div>
+    `;
 
 });
